ef3f3877fc2e4dbdcf5017c7eddd8d3a65c75818
fbimantt11 <fadhilahbimantoro@gmail.com>
2023-11-27 20:42:26 +0700

    update dashboard admin(pelan2)

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -9,6 +9,8 @@
           <i class="fas fa-users" style="font-size: 90px;"></i>
           <h3>Data Mahasiswa</h3>
           <h3>Total: 100</h3>
+          <a href="#" class="btn btn-light mt-3">More Info</a>
+          {{-- <a href= "{{ route('more_info_mahasiswa_perjurusan') }}" class="btn btn-light mt-3">More Info</a>  --}}
         </div>
       </div>
     </div>
@@ -19,6 +21,8 @@
           <i class="fas fa-graduation-cap" style="font-size: 90px;"></i>
           <h3>Data Jurusan</h3>
           <h3>Total: 10</h3>
+           <a href="#" class="btn btn-light mt-3">More Info</a>
+          {{-- <a href= "{{ route('more_info_mahasiswa_perjurusan') }}" class="btn btn-light mt-3">More Info</a>  --}}
         </div>
       </div>
     </div>
@@ -29,6 +33,8 @@
           <i class="fas fa-book" style="font-size: 90px;"></i>
           <h3>Data Mata Kuliah</h3>
           <h3>Total: 50</h3>
+           <a href="#" class="btn btn-light mt-3">More Info</a>
+          {{-- <a href= "{{ route('more_info_mahasiswa_perjurusan') }}" class="btn btn-light mt-3">More Info</a>  --}}
         </div>
       </div>
     </div>
@@ -38,7 +44,42 @@
         <div class="card-body">
           <i class="fas fa-user-graduate" style="font-size: 58px;"></i>
           <h3>Data Mahasiswa Perjurusan</h3>
-          <h3>Total: 30</h3> 
+          <h3>Total: 30</h3>
+            <a href="#" class="btn btn-light mt-3">More Info</a>
+          {{-- <a href= "{{ route('more_info_mahasiswa_perjurusan') }}" class="btn btn-light mt-3">More Info</a>  --}}
+        </div>
+      </div>
+    </div>
+
+
+    <div class="col-md-3 mt-3">
+      <div class="card text-left" style="background-color: #B99470; border-radius: 15px; color: white;">
+        <div class="card-body">
+          <i class="fas fa-users" style="font-size: 50px; float: left; margin-right: 10px;"></i>
+          <h5>Total User Aktif</h5>
+          <h5>Total: 150</h5> 
+        </div>
+      </div>
+    </div>
+
+ 
+    <div class="col-md-3 mt-3">
+      <div class="card text-left" style="background-color: #B99470; border-radius: 15px; color: white;">
+        <div class="card-body">
+          <i class="fas fa-user-shield" style="font-size: 50px; float: left; margin-right: 10px;"></i>
+          <h5>Total Admin User</h5>
+          <h5>Total: 50</h5> 
+        </div>
+      </div>
+    </div>
+
+
+    <div class="col-md-4 mt-3">
+      <div class="card text-left" style="background-color: #B99470; border-radius: 15px; color: white;">
+        <div class="card-body">
+          <i class="fas fa-user-graduate" style="font-size: 50px; float: left; margin-right: 10px;"></i>
+          <h5>Total Mahasiswa User</h5>
+          <h5>Total: 100</h5> 
         </div>
       </div>
     </div>
@@ -64,6 +105,8 @@
     </div>
   </div>
 </div>
+
+
 
 {{-- CDN --}}
 <script src="https://kit.fontawesome.com/your-fontawesome-kit.js" crossorigin="anonymous"></script>
