bcd7404f88df339387d5c94bb36623fad260a179
Nurul-anam <nhurulanam1234@gmail.com>
2023-11-30 00:16:29 +0700

    merubah sedikit

--- resources/views/students/student.blade.php
+++ resources/views/students/student.blade.php
@@ -4,7 +4,9 @@
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
     <div class="container">
-        <div class="card-header">{{ __('Selamat Datang ') }} {{ auth()->user()->name }}</div>
+        <h1 class="card-header text-center" style="color: #00C0EF">
+            {{ __('Selamat Datang') }} {{ auth()->user()->name }}
+        </h1>
         <div class="row">
             <div class="col">
                 <div class="card text-center">
@@ -19,7 +21,7 @@
                 <div class="card text-center" style="background-color: #00C0EF">
                     <div class="card-body">
                         <ion-icon name="people-outline" style="font-size: 90px; color: white"></ion-icon>
-                        <h3 class="text-white">Absensi</h3>
+                        <h3 class="text-white">Tugas</h3>
                         <h3 class="text-white">66</h3>
                     </div>
                 </div>
@@ -28,7 +30,7 @@
                 <div class="card text-center" style="background-color: #00C0EF">
                     <div class="card-body">
                         <ion-icon name="people-outline" style="font-size: 90px; color: white"></ion-icon>
-                        <h3 class="text-white">Keuangan</h3>
+                        <h3 class="text-white">Tugas</h3>
                         <h3 class="text-white">66</h3>
                     </div>
                 </div>
@@ -37,53 +39,10 @@
                 <div class="card text-center" style="background-color: #00C0EF">
                     <div class="card-body">
                         <ion-icon name="people-outline" style="font-size: 90px; color: white"></ion-icon>
-                        <h3 class="text-white">Nilai</h3>
+                        <h3 class="text-white">Tugas</h3>
                         <h3 class="text-white">66</h3>
                     </div>
                 </div>
-            </div>
-            <div class="container mt-5">
-                <h2 class="mb-4">Formulir Absensi</h2>
-
-                <form>
-                    <div class="form-row">
-                        <div class="form-group col-md-2">
-                            <label for="inputNo">No</label>
-                            <input type="text" class="form-control" id="inputNo" placeholder="No">
-                        </div>
-                        <div class="form-group col-md-4">
-                            <label for="inputNama">Nama</label>
-                            <input type="text" class="form-control" id="inputNama" placeholder="Nama">
-                        </div>
-                        <div class="form-group col-md-2">
-                            <label for="inputKelas">Kelas</label>
-                            <input type="text" class="form-control" id="inputKelas" placeholder="Kelas">
-                        </div>
-                        <div class="form-group col-md-3">
-                            <label for="inputKeterangan">Keterangan</label>
-                            <input type="text" class="form-control" id="inputKeterangan" placeholder="Keterangan">
-                        </div>
-                        <div class="form-group col-md-1">
-                            <label for="inputAksi">Aksi</label>
-                            <button type="button" class="btn btn-primary btn-block" onclick="submitForm()">Tambah</button>
-                        </div>
-                    </div>
-                </form>
-                <table class="table mt-4">
-                    <thead>
-                        <tr>
-                            <th scope="col">No</th>
-                            <th scope="col">Nama</th>
-                            <th scope="col">Kelas</th>
-                            <th scope="col">Keterangan</th>
-                            <th scope="col">Aksi</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <!-- Data absensi akan ditampilkan di sini -->
-                    </tbody>
-                </table>
-
             </div>
         </div>
     </div>
