1fc20b3ecd6ab8050eb5e1356df467cec8ad246e
Gittt13 <muhammad.1288@students.amikom.ac.id>
2023-11-30 08:39:54 +0700

    gatau apa

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,7 +55,8 @@
                             @endif
                         @else
                             <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre 
+                                style="color:#F3EEEA; font-family: 'Poppins', sans-serif;">
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
