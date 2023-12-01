8ce00123a8b4df36aeae3144e1f05a988a5d734a
fbimantt11 <fadhilahbimantoro@gmail.com>
2023-12-01 20:46:04 +0700

    UPDATE ADMIN ABOUT

--- resources/views/admin/about.blade.php
+++ resources/views/admin/about.blade.php
@@ -1,25 +1,23 @@
 @extends('layouts.admin')
 
-
-
 @section('content')
-<div class="container mt-5">
+
   <div class="row">
     <div class="col-md-8 offset-md-2 text-center">
       <h2 class="mb-4">Selamat Datang di Halaman Admin</h2>
       <p>
         Halaman ini merupakan pusat kendali untuk mengelola semua data mahasiswa, jurusan, mata kuliah, dan informasi penting lainnya di institusi pendidikan ini.
       </p>
-      <img src="https://png.pngtree.com/png-clipart/20230409/original/pngtree-admin-and-customer-service-job-vacancies-png-image_9041264.png" alt="Admin Dashboard" class="img-fluid ">
+      <img src="https://png.pngtree.com/png-clipart/20230409/original/pngtree-admin-and-customer-service-job-vacancies-png-image_9041264.png" alt="Admin Dashboard" class="img-fluid " style="max-width: 30%; height: auto;">
 
       <p>
-        Sebagai admin, Anda memiliki tanggung jawab untuk menjaga integritas data, menyediakan informasi yang akurat, dan memastikan semua proses administrasi berjalan lancar.
+        Sebagai admin, Anda memiliki tanggung jawab untuk menjaga integritas data, menyediakan informasi yang akurat, dan memastikan semua proses administrasi berjalan lancar.Terus tingkatkan kinerja Anda dan pastikan pengalaman pengguna yang optimal di setiap interaksi dengan dashboard ini.
       </p>
 
 
 
-      <p class="mt-4">
-        Terus tingkatkan kinerja Anda dan pastikan pengalaman pengguna yang optimal di setiap interaksi dengan dashboard ini.
+      <p >
+        
       </p>
 
       <div class="mt-5">
