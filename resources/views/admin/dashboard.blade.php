4532d25085748e4e9aa8d68f41d9cf40b2ce0c64
Gittt13 <muhammad.1288@students.amikom.ac.id>
2023-12-04 11:07:09 +0700

    penambahan kalender

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -134,6 +134,7 @@
     <div class="card">
       <div class="card-body">
         <h2 class="card-title">Kalender Kegiatan</h2>
+        <iframe src="https://calendar.google.com/calendar/embed?src=bd59f174b4bfb749b683385984d86799702b304b00bd01b983fd6b0a5c37fd68%40group.calendar.google.com&ctz=Asia%2FJakarta" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
         <div id="calendar"></div>
       </div>
     </div>
--- resources/views/students/kalender.blade.php
+++ resources/views/students/kalender.blade.php
@@ -2,5 +2,5 @@
 @extends('layouts.student')
 
 @section('content')
-<h2>ini kalender</h2>
+<iframe src="https://calendar.google.com/calendar/embed?src=bd59f174b4bfb749b683385984d86799702b304b00bd01b983fd6b0a5c37fd68%40group.calendar.google.com&ctz=Asia%2FJakarta" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
 @endsection
