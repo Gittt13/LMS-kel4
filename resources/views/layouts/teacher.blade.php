13784d2c5d6eedb76548190777e42ed0c81962d6
Gittt13 <muhammad.1288@students.amikom.ac.id>
2023-12-05 22:10:25 +0700

    mengedit home page

--- resources/views/layouts/teacher.blade.php
+++ resources/views/layouts/teacher.blade.php
@@ -265,7 +265,7 @@
                     <img src="{{ Auth::user()->avatar }}" class="img-circle elevation-2" alt="User Image">
                 </div>
                 <div class="info">
-                    <a href="{{ route('profile') }}" class="d-block">{{ Auth::user()->name }}</a>
+                    <a href="{{ route('profile') }}" class="d-block" style="text-decoration:none;">{{ Auth::user()->name }}</a>
                 </div>
             </div>
 
