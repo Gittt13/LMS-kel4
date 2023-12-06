f7fbc0ff3a831fcf7e054ea6fa23650be40fe85c
Nurul-anam <nhurulanam1234@gmail.com>
2023-12-06 12:27:57 +0700

    menambah nilai

--- resources/views/students/nilai.blade.php
+++ resources/views/students/nilai.blade.php
@@ -1,5 +1,35 @@
 @extends('layouts.student')
 
 @section('content')
-<h2>ini nilai</h2>
+<div class="container mt-5 bg-white p-4 rounded">
+    <h2>Tampilan Nilai</h2>
+    
+    <table class="table table-striped">
+        <thead class="table-dark">
+            <tr>
+                <th scope="col">No</th>
+                <th scope="col">Kelas</th>
+                <th scope="col">Nilai</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>Matematika</td>
+                <td>85</td>
+            </tr>
+            <tr>
+                <th scope="row">2</th>
+                <td>Bahasa Inggris</td>
+                <td>92</td>
+            </tr>
+            <tr>
+                <th scope="row">3</th>
+                <td>IPA</td>
+                <td>78</td>
+            </tr>
+            <!-- Tambahkan baris ini untuk setiap data nilai yang Anda miliki -->
+        </tbody>
+    </table>
+</div>
 @endsection
