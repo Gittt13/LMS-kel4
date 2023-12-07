12ab92498461bfab6c9d0eca979284778f255e34
Seiyainzu <seiyainzu@gmail>
2023-12-07 09:38:19 +0700

    edit teacher

--- resources/views/teacher/tugas_teacher.blade.php
+++ resources/views/teacher/tugas_teacher.blade.php
@@ -10,19 +10,19 @@
             <!-- Dashboard -->
             
             <div class="card-deck">
-                <a href="{{ route('kelas') }}" class="card text-center" style="background-color: #427D9D; border-radius: 15px; color: white; height: 75px;">
+                <a href="{{ route('kelas') }}" class="card text-center" style="background-color: #427D9D; border-radius: 15px; color: white; height: 75px; text-decoration:none;">
                     <div class="card-body d-flex flex-column align-items-center justify-content-center">
                         <ion-icon name="chatbubble-outline" style="font-size: 25px;"></ion-icon>
                         <h5 style="font-size: small;">Forum</h5>
                     </div>
                 </a>
-                <a href="{{ route('tugas.teacher') }}" class="card text-center " style="background-color: #427D9D; border-radius: 15px; color: white; height: 75px;">
+                <a href="{{ route('tugas.teacher') }}" class="card text-center " style="background-color: #427D9D; border-radius: 15px; color: white; height: 75px; text-decoration:none;">
                     <div class="card-body d-flex flex-column align-items-center justify-content-center">
                         <ion-icon name="document-outline" style="font-size: 25px;"></ion-icon>
                         <h5 style="font-size: small;">Tugas</h5>
                     </div>
                 </a>
-                <a href="{{ route('anggota') }}" class="card text-center" style="background-color: #427D9D; border-radius: 15px; color: white; height: 75px;">
+                <a href="{{ route('anggota') }}" class="card text-center" style="background-color: #427D9D; border-radius: 15px; color: white; height: 75px; text-decoration:none;">
                     <div class="card-body d-flex flex-column align-items-center justify-content-center">
                         <ion-icon name="people-outline" style="font-size: 25px;"></ion-icon>
                         <h5 style="font-size: small;">Anggota</h5>
